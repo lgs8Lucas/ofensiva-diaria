0231f2b5aab06f90182eaa8d30b2ab5b4e43e909
Lucas <lgs.08lucas@gmail.com>
2025-01-25 16:54:44 -0300

    Iniciado a criação de rotas da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
 
 function App() {
-  return (
-    <>
-      <h1>Ofensiva diária</h1>
-    </>
-  )
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
-export default App
+export default App;
