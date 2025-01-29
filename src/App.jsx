62c1cbafdc7f397af5a932b0fc7711e675141fbf
Lucas <lgs.08lucas@gmail.com>
2025-01-29 18:47:52 -0300

    Criando contexto para pegar usuário autenticado na aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
+
+import { AuthProvider } from "./context/AuthContext";
+
 import Home from "./pages/Home/Home";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
@@ -7,17 +14,34 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 function App() {
+	const [user, setUser] = useState(undefined);
+	const { auth } = useAuthentication();
+
+	const loadingUser = user === undefined;
+
+	useEffect(() => {
+		onAuthStateChanged(auth, (user) => {
+			setUser(user);
+		});
+	}, [auth]);
+
+	if (loadingUser) {
+		return <p>Carregando...</p>;
+	}
+
 	return (
 		<div id="app">
-			<BrowserRouter>
-				<Navbar />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/register" element={<Register />} />
-				</Routes>
-				<Footer />
-			</BrowserRouter>
+			<AuthProvider value={user}>
+				<BrowserRouter>
+					<Navbar />
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/register" element={<Register />} />
+					</Routes>
+					<Footer />
+				</BrowserRouter>
+			</AuthProvider>
 		</div>
 	);
 }
