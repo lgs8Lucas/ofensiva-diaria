7c4f1929d9122c47a3e92f7890a271a337cfc910
Lucas <lgs.08lucas@gmail.com>
2025-01-31 15:25:39 -0300

    Criado rodapé e alterado estilo da Navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import styles from "./Footer.module.css"
 
 const Footer = () => {
-  return (
-    <footer>
-      <p>Footer</p>
-    </footer>
-  )
-}
+	return (
+		<footer className={styles.footer}>
+			<h1>Ofensiva Diária</h1>
+      <p>Um site para você criar metas diárias e acompanha-las!</p>
+      <hr />
+			<p>
+				Criado por{" "}
+				<a href="https://www.linkedin.com/in/lucas-silva-tech/" target="_blank">
+					Lucas Gonçalves
+				</a>
+				, acompanhe mais projetos meus no meu{" "}
+				<a href="https://github.com/lgs8Lucas" target="_blank">
+					GitHub
+				</a>!
+			</p>
+		</footer>
+	);
+};
 
-export default Footer
+export default Footer;
